cb2855673748b337f1c07c4c7f960d3abd3944c7
Jonathan Xiao <jonathanx111@gmail.com>
2020-09-02 13:21:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  # if name_hash == {}
+  #   return nil 
+  # end
+  
+  new1 = ""
+  name_hash.each do |key, value|
+    new = value
+    if value < new
+      new = value 
+      new1 = key
+    end
+  end
+   new1 
 end
+puts key_for_min_value({:chair => 25, :table => 85, :mattress => 450})
